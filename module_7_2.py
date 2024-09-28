1f3a093c11db6997b2c400aad622e7ecbaf7fa9b
Anatoliy_Yavorskiy <yardbirds1957@gmail.com>
2024-09-28 17:21:15 +0300

    7_2

--- module_7_2.py
+++ module_7_2.py
@@ -11,14 +11,14 @@ info = [
 def read_str():
     """Функция открывает файл "module_7_2.txt", считывает содержимое и закрывает файл.
     """
-    file_read_str = open("module_7_2.txt", "r")
+    file_read_str = open("module_7_2.txt", "r", encoding="utf-8")
     try:
         return file_read_str.read()
     finally:
         file_read_str.close()
 
 
-def custom_write(file_name, strings):
+def custom_write(file_name, *args, strings=None):
     """Функция принимает аргументы file_name - название файла для записи, strings - список строк для записи.
        Функция должна:
        - записывать в файл file_name все строки из списка strings, каждая на новой строке;
@@ -27,9 +27,11 @@ def custom_write(file_name, strings):
        - Для получения номера байта начала строки используйте метод tell() перед записью;
     """
     strings_index = {}
+    if strings is None:
+        strings = [*args]
     for sub_string in strings:
         if sub_string in read_str():
-            print("isinstance")
+            print(f"субстрока {sub_string} уже есть в файле, запишите иначе")
         else:
             file = open(file_name, "a", encoding="utf-8")
             num_byte = file.tell()
@@ -38,12 +40,12 @@ def custom_write(file_name, strings):
             file = open(file_name, "r", encoding="utf-8")
             for item, line in enumerate(file):
                 a = (item + 1)
-            num_str = a
-            strings_index.update({(num_str, num_byte): sub_string})
+            count_str = a
+            strings_index.update({(count_str, num_byte): sub_string})
             file.close()
 
     print(strings_index)
 
 
-custom_write('module_7_2.txt', info)
+custom_write('module_7_2.txt', 'board', '12 packs', 'Привет со строки три!', 'а это уже четвёртая строка')
 
